d474922c163650c9af5a31f7a3740594828fb716
yogamalabe <124970337+yogamalabe@users.noreply.github.com>
2023-02-28 20:54:59 +0530

    Add files via upload

---
+++ script07.js
@@ -0,0 +1,30 @@
+var request=new XMLHttpRequest();
+request.open("GET","https://restcountries.com/v2/all");
+request.send();
+request.onload=function(){
+    var result=JSON.parse(request.response);
+   // a) Get all the countries from the Asia continent /region using the Filter functiona) 
+    var reg=result.filter((ele)=>ele.region==='Asia');
+    console.log(reg);
+
+    // b) Get all the countries with a population of less than 2 lakhs using Filter function
+      var pop=result.filter((ele)=>ele.population<200000);
+      console.log(pop);
+    
+    //c)Print the following details name, capital, flag using forEach function
+        result.forEach(element => {
+            console.log(element.name);
+            console.log(element.capital);
+            console.log(element.flag);
+           });
+    
+    //D) Print the total population of countries using reduce function
+       var totalpop=result.reduce((a,b)=>a+b.population,0);
+       console.log(totalpop);
+
+  //E)Print the country which uses US Dollars as currency
+    //var arr=result.map(ele=>ele.currencies);
+    //console.log(arr);
+
+}
+
